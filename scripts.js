3dd4201489fcb8e4801cbe4393f0bdb7700fd16a
alx9601 <fer_alexei196@hotmail.com>
2024-03-14 14:48:41 -0500

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,89 @@
+//SCROLL TO ABOUT ME
+
+document.addEventListener('DOMContentLoaded', function() {
+    const aboutMeLink = document.querySelector('a[href="#about-me"]');
+
+    aboutMeLink.addEventListener('click', function(event) {
+        event.preventDefault(); // Evita el comportamiento predeterminado del enlace (navegar a otra página)
+
+        const aboutMeSection = document.getElementById('about-me');
+        const sectionOffset = aboutMeSection.offsetTop; // Obtén la distancia desde la parte superior del documento hasta la sección "About Me"
+
+        window.scrollTo({
+            top: sectionOffset,
+            behavior: 'smooth' // Realiza un scroll suave
+        });
+    });
+});
+
+
+//SLIDER
+
+// Selecciona todas las imágenes dentro del slider
+const slides = document.querySelectorAll('.slider img');
+let currentSlide = 0;
+
+// Función para mostrar el siguiente slide
+function nextSlide() {
+  slides[currentSlide].style.opacity = 0; // Oculta el slide actual
+  currentSlide = (currentSlide + 1) % slides.length; // Incrementa el índice del slide actual
+  slides[currentSlide].style.opacity = 1; // Muestra el nuevo slide
+}
+
+// Mostrar el primer slide
+slides[currentSlide].style.opacity = 1;
+
+// Configurar un intervalo para cambiar de slide automáticamente
+setInterval(nextSlide, 7000); // Cambia de slide cada 7 segundos (7000 milisegundos)
+
+//VIEW MORE
+
+document.addEventListener('DOMContentLoaded', function() {
+    const moreButton = document.getElementById('view-more-btn');
+    const cardContainer = document.querySelector('.card-gallery');
+    const initialCardCount = 6; // Número inicial de cards
+
+    moreButton.addEventListener('click', function() {
+        // Crear tres nuevas cards
+        for (let i = 0; i <3 ; i++) {
+            const newCard = document.createElement('div');
+            newCard.classList.add('card');
+            newCard.innerHTML = `
+                <img src="imagenes/imagen3.png" alt="Work ${i+10}">            
+                 <h3>Work ${i+10}</h3>            
+                 `;
+        
+
+            // Agrega la nueva card al contenedor de cards
+            cardContainer.appendChild(newCard);
+
+            // Mostrar la nueva card con una pequeña demora para permitir que se aplique la transición
+            setTimeout(() => {
+                newCard.style.transform = 'translateY(0)'; // Desplazar hacia abajo
+            }, 50 * i);
+        }
+    });
+});
+
+//BACK TO TOP
+
+// Función para desplazarse suavemente hacia la parte superior de la página
+
+document.addEventListener('DOMContentLoaded', function() {
+    const scrollToTopButton = document.getElementById('back-to-top');
+
+    scrollToTopButton.addEventListener('click', function() {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth' // Desplazamiento suave
+        });
+    });
+
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > 300) {
+            scrollToTopButton.style.display = 'block';
+        } else {
+            scrollToTopButton.style.display = 'none';
+        }
+    });
+});
